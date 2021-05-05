371f4a65aace9b3dbb9d47e40c2ee883ef05e78b
Mike S <mschoenike1@my.wctc.edu>
2021-05-05 18:28:32 -0500

    Initial commit

---
+++ haiku.js
@@ -0,0 +1,10 @@
+let dictionary = [[],[],[],[],[],[],[],[]];
+dictionary[1] = ["a", "the", "my", "of", "true", "false", "love", "heart", "pain", "grow", "hate",
+    "through", "war","soul","spring","fall"];
+dictionary[2] = ["over", "under","around", "table", "romance","unjust","soda","tonic","carpet", "triumph",
+    "oven", "summer", "autumn","winter"];
+dictionary[3] = ["victory","computer","elusive","triumphant","increasing","telephone","strategy"];
+dictionary[4] = ["elevator","degenerate","copulation","execution","dictionary","indecisive"];
+dictionary[5] = ["enthusiastic","subterranean","excruciating","terminology","choreography","teleportation"];
+dictionary[6] = ["intelligentsia","humanitarian","characterization","identification","accountability"];
+dictionary[7] = ["establishmentarian","onomatopoetic", "perpendicularity"]
