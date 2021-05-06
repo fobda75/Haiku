24d2589b7b20dd46ca94b596da7c2bab2ff2e354
Mike S <mschoenike1@my.wctc.edu>
2021-05-05 21:53:14 -0500

    updated files... ready for final testing

--- haiku.js
+++ haiku.js
@@ -8,3 +8,51 @@ dictionary[4] = ["elevator","degenerate","copulation","execution","dictionary","
 dictionary[5] = ["enthusiastic","subterranean","excruciating","terminology","choreography","teleportation"];
 dictionary[6] = ["intelligentsia","humanitarian","characterization","identification","accountability"];
 dictionary[7] = ["establishmentarian","onomatopoetic", "perpendicularity"]
+$(document).ready(function(){
+    $("#submitWord").on("click",addWord);
+    $("#haikuGenerate").on("click",generateHaiku);
+})
+
+function addWord(event){
+    event.preventDefault();
+    let splitWord=$("#wordEntry").val();
+    let wordArray = splitWord.split('-');
+    let wordSyllables = wordArray.length;
+    if(wordSyllables>7){
+        $("#wordEntryCorrection").text("The maximum number of syllables permitted is seven.");
+    }
+    else{
+        $("#wordEntryCorrection").text("");
+        let newWord = wordArray.join("");
+        dictionary[wordSyllables].push(newWord);
+
+        $("#wordEntry").val("");
+    }
+
+}
+
+function generateHaiku(){
+    $("#haikuLineOne").empty();
+    $("#haikuLineTwo").empty();
+    $("#haikuLineThree").empty();
+    let line1 = generateLine(5);
+    let line2 = generateLine(7);
+    let line3 = generateLine(5);
+    $("#haikuLineOne").text(line1);
+    $("#haikuLineTwo").text(line2);
+    $("#haikuLineThree").text(line3);
+}
+
+function generateLine(syllables){
+    let remainingSyllables = syllables;
+    let wordLength;
+    let output="";
+    let selectIndex;
+    while (remainingSyllables>0){
+        wordLength = Math.floor((Math.random()*remainingSyllables)+1);
+        selectIndex= Math.floor(Math.random()*dictionary[wordLength].length);
+        output += `${dictionary[wordLength][selectIndex]} `;
+        remainingSyllables -= wordLength;
+    }
+    return output;
+}
